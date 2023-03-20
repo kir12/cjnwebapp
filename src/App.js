e0c45831cd7b27781cc73241eccdb50b214a9a04
Brian Lee <blee025432@gmail.com>
2023-03-20 19:04:52 -0400

    begin impl of bookmarks

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 import { readCSV, DataFrame, toJSON, Dt, toDateTime, Series} from 'danfojs';
 import events from './events.tsv';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Badge from 'react-bootstrap/Badge';
 import { useEffect, useState } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { colors } from "./Utils.js"
 import dayjs from 'dayjs';
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+
 dayjs.extend(customParseFormat)
+
+function Bookmark() {
+  const [starType, setStarType] = useState(faStar);
+
+  function handleOnClick(){
+    // TODO: add cookie
+    if(starType === faStar){
+      setStarType(fasStar);
+    }
+    else{
+      setStarType(faStar);
+    }
+  } 
+
+  return (<FontAwesomeIcon onClick = {handleOnClick} icon={starType} size="lg" />);
+}
 
 function Dataset() {
   const [dataSet, setDataSet] = useState(new DataFrame());
@@ -34,8 +56,6 @@ function Dataset() {
         data = data.addColumn("combinedEnd",data.apply(combineTimeColumnsEnd, {axis:1}))
         data = data.sortValues("combinedStart",{ascending:true});
 
-        data.print();
-
         // optional: drop original time parameters
 
         setDataSet(data);
@@ -47,21 +67,9 @@ function Dataset() {
 
   });
 
-
-  function event_age_badge(type){
-    if(type !== null){
-      return (<></>);
-    }
-    else{
-      return (<></>);
-    }
-  }
-
   let output = [];
 
   if (dataUpdated) {
-
-    dataSet.print();
 
     let event_types = toJSON(dataSet["event_type"].unique())[0];
 
@@ -90,10 +98,17 @@ function Dataset() {
       // generate event listing
       output.push(
         <ListGroup.Item key={index}>
-          <h3>{elem["event_title"]} </h3>
-          <p>{elem["event_room"]}, {startjs.toString()} - {endjs.toString()}</p>
-          <p><Badge pill className={css_class}>{elem["event_type"]}</Badge> <Badge pill bg="danger">{elem["event_age_limit"]}</Badge></p>
-        </ListGroup.Item>
+          <Row>
+            <Col xs="10">
+              <h3>{elem["event_title"]} </h3>
+              <p>{elem["event_room"]}, {startjs.toString()} - {endjs.toString()}</p>
+              <p><Badge pill className={css_class}>{elem["event_type"]}</Badge> <Badge pill bg="danger">{elem["event_age_limit"]}</Badge></p>
+            </Col>
+            <Col xs="2" className="text-center align-self-center">
+              <Bookmark></Bookmark>
+            </Col>
+          </Row>
+       </ListGroup.Item>
       );
     });
 
