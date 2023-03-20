e959c09ca85e80ae200922a4678969c8cf2738ac
Brian Lee <blee025432@gmail.com>
2023-03-20 18:13:38 -0400

    implement times and 18+ badge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { readCSV, DataFrame, toJSON, concat} from 'danfojs';
+import { readCSV, DataFrame, toJSON, Dt, toDateTime, Series} from 'danfojs';
 import events from './events.tsv';
 import Container from 'react-bootstrap/Container';
 import Badge from 'react-bootstrap/Badge';
 import { useEffect, useState } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { colors } from "./Utils.js"
-
+import dayjs from 'dayjs';
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat)
 
 function Dataset() {
   const [dataSet, setDataSet] = useState(new DataFrame());
   const [dataUpdated, setDataUpdated] = useState(false);
 
+  function combineTimeColumnsStart(row) {
+    return dayjs(row[3] + " " + row[4], "M/D/YY H:mm").toISOString();
+  }
+
+  function combineTimeColumnsEnd(row) {
+    return dayjs(row[5] + " " + row[6], "M/D/YY H:mm").toISOString();
+  }
+
   useEffect(() => {
 
     const fetchData = async () => {
-      const data = await readCSV(events);
+      let data = await readCSV(events);
       // data.head().print();
       if (!dataUpdated) {
+
+        data = data.addColumn("combinedStart",data.apply(combineTimeColumnsStart, {axis:1}))
+        data = data.addColumn("combinedEnd",data.apply(combineTimeColumnsEnd, {axis:1}))
+        data = data.sortValues("combinedStart",{ascending:true});
+
+        data.print();
+
+        // optional: drop original time parameters
+
         setDataSet(data);
         setDataUpdated(true);
       }
@@ -29,29 +48,56 @@ function Dataset() {
   });
 
 
-  console.log(dataSet.index);
+  function event_age_badge(type){
+    if(type !== null){
+      return (<></>);
+    }
+    else{
+      return (<></>);
+    }
+  }
+
   let output = [];
 
   if (dataUpdated) {
 
-    let event_types = dataSet["event_type"].unique();
-    let num_event_types = dataSet["event_type"].nUnique();
-    let badgecolors = concat({dfList: [event_types, colors.head(num_event_types)], axis:1}) //.setIndex({column:"event_type", drop:true});
+    dataSet.print();
 
+    let event_types = toJSON(dataSet["event_type"].unique())[0];
+
+    
     let jsonexport = toJSON(dataSet);
     jsonexport.forEach(function (elem, index) {
-      let bg_color = badgecolors.iloc(elem["event_type"])["bg"];
-      let text_color = badgecolors.iloc(elem["event_type"])["text"];
+
+      // compute event type badge styling
+      let evttype = (elm) => elm === elem["event_type"];
+      let event_idx = event_types.findIndex(evttype);
+      let css_class = colors[event_idx];
+
+      // compute time display
+      let startjs = dayjs(elem["combinedStart"]);
+      let endjs = dayjs(elem["combinedEnd"]);
+      let format_str = "";
+      if(startjs.day() !== endjs.day()){
+        format_str = "D/M h:mm A";
+      }
+      else{
+        format_str = "h:mm A";
+      }
+      startjs = startjs.format(format_str);
+      endjs = endjs.format(format_str);
+
+      // generate event listing
       output.push(
         <ListGroup.Item key={index}>
-          <h2>{elem["event_title"]} <span ><Badge pill style={{ backgroundColor: 'red !important', color:'white !important'}}>{elem["event_type"]}</Badge></span></h2>
+          <h3>{elem["event_title"]} </h3>
+          <p>{elem["event_room"]}, {startjs.toString()} - {endjs.toString()}</p>
+          <p><Badge pill className={css_class}>{elem["event_type"]}</Badge> <Badge pill bg="danger">{elem["event_age_limit"]}</Badge></p>
         </ListGroup.Item>
       );
     });
 
   }
-
-  // let df_display = dataSet[["event_title","event_type"]].apply(createEntry, {axis: 1});
 
   return (
     <ListGroup>
--- src/Utils.js
+++ src/Utils.js
@@ -1,19 +1,3 @@
-import { DataFrame } from "danfojs";
+import { Series } from "danfojs";
 
-export let colors = new DataFrame(
-    [
-    {"bg": "aqua", "text":"white"},
-    {"bg": "purple","text":"white"},
-    {"bg": "fuschia","text":"white"},
-    {"bg": "green","text":"white"},
-    {"bg": "lime","text":"white"},
-    {"bg": "olive","text":"white"},
-    {"bg": "yellow","text":"black"},
-    {"bg": "navy","text":"white"},
-    {"bg": "blue","text":"white"},
-    {"bg": "teal","text":"white"},
-    {"bg": "black","text":"white"},
-    {"bg": "silver","text":"black"},
-    {"bg": "gray","text":"black"},
-    ]
-);
+export let colors = ["purple","fuchsia","green","lime","olive","yellow","navy","blue","teal","black","silver","gray", "aqua"];
